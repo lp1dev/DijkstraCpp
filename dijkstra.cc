3807ccbc8a11cdcf09aec1b2d9a2e8f22ede4075
Lupin <lp1@lp1.eu>
2016-12-03 18:06:51 +0100

    Stopped useless computing when we had a longer distance than the one stored

--- dijkstra.cc
+++ dijkstra.cc
@@ -1,19 +1,14 @@
 #define DEBUG 1
-#define OUTPUT std::cout
+#define OUTPUT std::cout << "\t"
+#define GRN "\033[32;32m"
+#define STD "\033[0;0m"
+
 #include "dijkstra.h"
 
-Dijkstra::Dijkstra(const Graph *graph, const vector<double> *arc_lengths) {
-    this->graph = graph;
-    this->arc_lengths = arc_lengths;
+Dijkstra::Dijkstra(const Graph *graph, const vector<double> *arc_lengths) : graph(graph), arc_lengths(arc_lengths) {
     for (int i = 0; i <= arc_lengths->size(); i++) {
         parentarcs.push_back(-1);
         distances.push_back(infinity);
-    }
-    if (DEBUG) {
-        for (int i = 0; i < arc_lengths->size(); i++) {
-            OUTPUT << "[" << arc_lengths->at(i) << "]";
-        }
-        OUTPUT << std::endl;
     }
 }
 
@@ -23,10 +18,6 @@ const Graph &Dijkstra::GetGraph() const {
 
 double Dijkstra::GetDistance(int source, int target) const {
     int id = graph->GetArcId(source, target);
-    if (DEBUG) {
-        OUTPUT << "Arc id :" << id << std::endl;
-        OUTPUT << "Arc length : " << arc_lengths->at(id) << std::endl;
-    }
     return arc_lengths->at(id);
 }
 
@@ -41,10 +32,9 @@ bool Dijkstra::IsNodeReached(int node) {
 void Dijkstra::RunForTarget(int seeked_source, int source, int target, double totalDistance, int targetId) {
     if (!resolvedTargets[targetId]) {
         if (DEBUG) {
-            OUTPUT << "Source: " << source << std::endl;
-            OUTPUT << "Target: " << target << std::endl;
+            OUTPUT << "/////\n\tFrom: (" << source << ") to (" << target << ") totalDistance :" << totalDistance
+                   << std::endl;
         }
-
         //Retrieving outgoing arcs from source
         vector<int> outgoing_arcs = graph->OutgoingArcs(source);
         vector<int> incoming_arcs = graph->IncomingArcs(source);
@@ -53,24 +43,28 @@ void Dijkstra::RunForTarget(int seeked_source, int source, int target, double to
         for (int i = 0; i < outgoing_arcs.size(); i++) {
             int arcFrom = graph->tail_[outgoing_arcs[i]];
             int arcTo = graph->head_[outgoing_arcs[i]];
-            if (DEBUG) {
-                OUTPUT << "Outgoing arc from source id: " << outgoing_arcs[i] << std::endl;
-                OUTPUT << "Incoming arc from source id: " << incoming_arcs[i] << std::endl;
-                OUTPUT << "Outgoing arc to:" << arcTo << std::endl;
-            }
             double distance = GetDistance(arcFrom, arcTo);
+            //If the distance we computed is longer, we stop here
+            if (distance > distances[arcTo])
+                continue;
+            if (DEBUG) {
+                OUTPUT << "(" << arcFrom << ") -> (" << arcTo << ") = " << distance << std::endl;
+            }
             totalDistance += distance;
-            distances[arcTo] = totalDistance; //We add totalDistance to distances
-            if (DEBUG)
-                OUTPUT << "assert distances[" << arcTo << "] = " << distances[arcTo] << std::endl;
-            parentarcs[arcTo] = outgoing_arcs[i]; //We add the arc to parentarcs
             //We add the target to reachedNodes if it hasn't been reached already
             if (!IsNodeReached(outgoing_arcs[i]))
                 reachedNodes.push_back(outgoing_arcs[i]);
             if (!IsNodeReached(incoming_arcs[i]))
                 reachedNodes.push_back(incoming_arcs[i]);
+            //We add totalDistance to distances
+            distances[arcTo] = totalDistance;
+            if (DEBUG)
+                OUTPUT << "assert distances[" << arcTo << "] = " << distances[arcTo] << std::endl;
+            //We add the arc to parentarcs
+            parentarcs[arcTo] = outgoing_arcs[i];
             //And keep seeking until we find the last node to the target
             if (arcTo == target) {
+                if (DEBUG) OUTPUT << GRN << "[Target " << target << " found]\n" << STD;
                 resolvedTargets[targetId] = 1;
             } else
                 RunForTarget(seeked_source, arcTo, target, totalDistance, targetId);
@@ -81,8 +75,10 @@ void Dijkstra::RunForTarget(int seeked_source, int source, int target, double to
 void Dijkstra::RunUntilAllTargetsAreReached(int source, const vector<int> &targets) {
     //Initialising the distance to the source to 0
     distances[source] = 0;
-    if (DEBUG)
+    if (DEBUG) {
+        OUTPUT << "--------------------------\n";
         OUTPUT << "Distance with " << source << " = 0\n";
+    }
     //Running for each target
     for (int i = 0; i < targets.size(); i++) {
         resolvedTargets.push_back(0);
@@ -90,10 +86,8 @@ void Dijkstra::RunUntilAllTargetsAreReached(int source, const vector<int> &targe
     }
     //Displaying the distances in DEBUG mode
     if (DEBUG) {
-        OUTPUT << "Distances :" << std::endl;
-        for (int i = 0; i < distances.size(); i++) {
-            OUTPUT << "[" << distances[i] << "]";
-        }
+        OUTPUT << "Distances : \n\t";
+        for (int i = 0; i < distances.size(); i++) std::cout << "[" << distances[i] << "]";
         OUTPUT << std::endl;
     }
 }
@@ -108,9 +102,9 @@ const vector<double> &Dijkstra::Distances() const {
 
 const vector<int> &Dijkstra::ParentArcs() const {
     if (DEBUG) {
-        OUTPUT << "ParentArcs :" << std::endl;
+        OUTPUT << "ParentArcs :\n\t";
         for (int i = 0; i < parentarcs.size(); i++) {
-            OUTPUT << "[" << parentarcs[i] << "]";
+            std::cout << "[" << parentarcs[i] << "]";
         }
         OUTPUT << std::endl;
     }
